c04b9eac34dd72ff0fddfad7ae28d05c1804cd6f
Anshuman Garg <anshumangarg120@gmail.com>
2023-09-27 11:42:10 +0530

    complite add friend button

--- Insta-features/script.js
+++ Insta-features/script.js
@@ -1,8 +1,25 @@
-var istatus = document.querySelector("h5")
+/** @format */
 
-var addFriend = document.querySelector("#add")
+var istatus = document.querySelector("h5");
 
-addFriend.addEventListener("click", function () {
-    istatus.innerHTML = "Friends"
-    istatus.style.color = "green"
-})
+var btn = document.querySelector("#add");
+
+var check = 0;
+
+btn.addEventListener("click", function () {
+  if (check == 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+    btn.innerHTML = "Remove";
+    btn.style.backgroundColor = "#dadada";
+    btn.style.color = "black";
+    check = 1;
+  } else {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "red";
+    btn.innerHTML = "Add Friend";
+    btn.style.backgroundColor = "cadetblue";
+    btn.style.color = "#fff";
+    check = 0;
+  }
+});
