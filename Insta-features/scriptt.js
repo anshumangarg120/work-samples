a34b853be379695cbc1faf4924e8070b34fc8d94
Anshuman Garg <anshumangarg120@gmail.com>
2023-10-16 15:54:51 +0530

    modify js

--- Insta-features/scriptt.js
+++ Insta-features/scriptt.js
@@ -1,16 +1,15 @@
-/** @format */
-
 var con = document.querySelector("#container")
 var love = document.querySelector("i")
 
 con.addEventListener("dblclick", function () {
-    love.style.transform = 'translate(-50%, -50%) scale(1)';
-    love.style.opacity = 0.8;
-     setTimeout(function () {
-       love.style.opacity = 0;
-     }, 1000);
+    love.style.transform = "translate(-50%, -50%) scale(1)";
+  // --- 
     setTimeout(function () {
-        love.style.transform = 'translate(-50%, -50%) scale(0)';
+        love.style.transform = "translate(-50%, -50%) scale(0)";
     },3000);
 });
-
+// ---
+//   love.style.opacity = 0.8;
+//   setTimeout(function () {
+//     love.style.opacity = 0;
+//   }, 1000);
